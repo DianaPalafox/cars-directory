e741856963600209ea9f14cf991a799d0e32aaae
Diana Palafox <dianapalafox95@gmail.com>
2023-02-25 16:18:38 -0700

    Create the functions to add cars and delete them

--- cars/src/store/index.js
+++ cars/src/store/index.js
@@ -1,19 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { carsReducer, addCar, removeCar, changeSearchTerm } from "./slices/carsSlice";
-import { formReducer, changeCost, changeName } from "./slices/formSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  carsReducer,
+  addCar,
+  removeCar,
+  changeSearchTerm,
+} from './slices/carsSlice';
+import { formReducer, changeName, changeCost } from './slices/formSlice';
 
 const store = configureStore({
-    reducer: {
-        cars: carsReducer,
-        form: formReducer
-    }
-})
+  reducer: {
+    cars: carsReducer,
+    form: formReducer,
+  },
+});
 
-export {
-    store, 
-    changeCost,
-    changeName,
-    addCar,
-    removeCar,
-    changeSearchTerm
-}
+export { store, changeName, changeCost, addCar, removeCar, changeSearchTerm };
--- cars/src/store/slices/carsSlice.js
+++ cars/src/store/slices/carsSlice.js
@@ -1,31 +1,30 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const carsSlice = createSlice({
-    name: 'cars',
-    initialState:{
-        searchTerm: '',
-        cars: []
+  name: 'cars',
+  initialState: {
+    searchTerm: '',
+    data: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
     },
-    reducers:{
-        changeSearchTerm(state, action) {
-            state.searchTerm = action.payload;
-        },
-        addCar(state, action) {
-            state.cars.push({
-                name: action.payload.name,
-                cost: action.payload.cost,
-                id: nanoid()
-            });
-        },
-        removeCar(state, action) {
-            const updated = state.cars.filter((car) => {
-                return car.id !== action.payload;
-            })
-            state.cars = updated;
+    addCar(state, action) {
+      state.data.push({
+        name: action.payload.name,
+        cost: action.payload.cost,
+        id: nanoid(),
+      });
+    },
+    removeCar(state, action) {
+      const updated = state.data.filter((car) => {
+        return car.id !== action.payload;
+      });
+      state.data = updated;
+    },
+  },
+});
 
-        },
-    }
-})
-
-export const { changeSearchTerm, addCar, removeCar } = carsSlice;
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
 export const carsReducer = carsSlice.reducer;
